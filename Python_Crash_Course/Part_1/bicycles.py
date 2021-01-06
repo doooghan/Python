c9f2f25bdc9b0dfbb8184a87c22d0a633f74cc2c
doooghan <doooghan@foxmail.com>
2021-01-06 23:42:33 +0800

    list

---
+++ Python_Crash_Course/Part_1/bicycles.py
@@ -0,0 +1,9 @@
+bicycles = ["trek", 'cannondale', 'redline', 'specialized', 5]
+print(bicycles)
+
+print(bicycles[0].title())
+print(bicycles[3].upper())
+print(bicycles[-1])
+
+message = "My first bicycle was a " + bicycles[0].title() + "."
+print(message)
