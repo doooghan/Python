5fe3bb5594f401f1b707849b65913edd55c3ef6c
doooghan <doooghan@foxmail.com>
2021-01-08 19:53:50 +0800

    easy

---
+++ Python_Crash_Course/Part_1/List/3_4_guests.py
@@ -0,0 +1,33 @@
+guests = ['hdd', 'yhh', 'hpp']
+
+print(guests)
+
+guests[1] = 'abb'
+print(guests)
+
+guests.insert(0, 'zjj')
+print(guests)
+
+guests.insert(2, 'znn')
+print(guests)
+
+guests.append('csh')
+print(guests)
+
+guests.pop()
+print(guests)
+
+guests.pop()
+print(guests)
+
+guests.pop()
+print(guests)
+
+guests.pop()
+print(guests)
+
+del guests[0]
+print(guests)
+
+del guests[0]
+print(guests)
