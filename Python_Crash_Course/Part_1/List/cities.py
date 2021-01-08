1e001fd39511f3a34f805b5f35fbaa66aa33891b
doooghan <doooghan@foxmail.com>
2021-01-08 21:02:20 +0800

    list_cities

---
+++ Python_Crash_Course/Part_1/List/cities.py
@@ -0,0 +1,21 @@
+cities = ['beijing', 'xian', 'hangzhou', 'shenzhen', 'shanghai']
+print(cities)
+
+print(sorted(cities))
+
+print(cities)
+
+print(sorted(cities, reverse=True))
+print(cities)
+
+cities.reverse()
+print(cities)
+
+cities.reverse()
+print(cities)
+
+cities.sort(reverse=True)
+print(cities)
+
+cities.sort()
+print(cities)
