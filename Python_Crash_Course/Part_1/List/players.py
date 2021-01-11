2d51cfb558a83eb55a0bb94d8c2b16ca237e6101
doooghan <doooghan@foxmail.com>
2021-01-11 20:45:28 +0800

    list_slice

---
+++ Python_Crash_Course/Part_1/List/players.py
@@ -0,0 +1,9 @@
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[:3])
+print(players[1:4])
+print(players[:4])
+
+print("The first three items in the list are:", players[:3])
+print("Three items from the middle of the list are:", players[1:4])
+print("The last three items in the list are:", players[-3:])
+
