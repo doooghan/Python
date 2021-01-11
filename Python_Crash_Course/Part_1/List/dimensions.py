38aaedd48010d666fa39f085c3f23f9c49987896
doooghan <doooghan@foxmail.com>
2021-01-11 20:50:20 +0800

    tuple

---
+++ Python_Crash_Course/Part_1/List/dimensions.py
@@ -0,0 +1,17 @@
+dimensions = (200, 50)
+print(dimensions[0])
+print(dimensions[1])
+
+# dimensions[1] = 100
+
+for dimension in dimensions:
+    print(dimension)
+
+print("Original dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400, 100)
+print("\nModified dimensions")
+for dimension in dimensions:
+    print(dimension)
