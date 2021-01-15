e45358b03e922b48bd82b7dfccc09e08b334c34e
doooghan <doooghan@foxmail.com>
2021-01-15 23:04:00 +0800

    dict_items_keys

--- Python_Crash_Course/Part_1/Chapter_6/favorite_languages.py
+++ Python_Crash_Course/Part_1/Chapter_6/favorite_languages.py
@@ -5,6 +5,35 @@ favorite_languages = {
     'phil': 'python',
     }
 
-print("Sarah's favorite language is " +
-      favorite_languages['sarah'].title() +
-      ".")
+# print("Sarah's favorite language is " +
+#       favorite_languages['sarah'].title() +
+#       ".")
+
+"""
+for name, language in favorite_languages.items():
+    print(name.title() + "'s favorite language is " +
+          language.title() + ".")
+
+print()
+
+for name in favorite_languages:
+    print(name.title())
+
+print()
+
+for name in favorite_languages.keys():
+    print(name.title())
+"""
+
+
+friends = ['phil', 'sarah']
+for name in favorite_languages:
+    print(name.title())
+
+    if name in friends:
+        print("\tHi " + name.title() +
+              ", I see your favorite language is " +
+              favorite_languages[name].title() + "!")
+
+if 'erin' not in favorite_languages.keys():
+    print("Erin, please take our poll")
