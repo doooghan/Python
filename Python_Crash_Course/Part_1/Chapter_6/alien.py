19e77b3d0c0a000d423112717c05abda4a4b0a48
doooghan <doooghan@foxmail.com>
2021-01-15 22:34:33 +0800

    alien

--- Python_Crash_Course/Part_1/Chapter_6/alien.py
+++ Python_Crash_Course/Part_1/Chapter_6/alien.py
@@ -1,3 +1,30 @@
-alien_0 = {'color': 'green', 'points':5}
+alien_0 = {'color': 'green', 'points': 5}
 print(alien_0['color'])
 print(alien_0['points'])
+print(alien_0)
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 0
+print(alien_0)
+
+print("This alien is " + alien_0['color'] + ".")
+
+alien_0['color'] = 'yellow'
+print("This alien is now " + alien_0['color'] + ".")
+
+alien_0['speed'] = 'medium'
+print("Original x-position: " + str(alien_0['x_position']))
+
+if alien_0['speed'] == "slow":
+    x_increment = 1
+elif alien_0['speed'] == "medium":
+    x_increment = 2
+else:
+    x_increment = 3
+
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+
+print("New x-positionis " + str(alien_0['x_position']))
+
+del alien_0['points']
+print(alien_0)
