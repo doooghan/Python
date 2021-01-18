8de2737ceb0f05d1ff89b717ab9958477af41d9e
doooghan <doooghan@foxmail.com>
2021-01-19 00:54:59 +0800

    input

---
+++ Python_Crash_Course/Part_1/Chapter_7/greeter.py
@@ -0,0 +1,10 @@
+"""
+name = input("Please enter your name: ")
+print("Hello ," + name + "!")
+"""
+
+prompt = "If you tell us who you are, we can personalize the message you see."
+prompt += "\nwhat's your first name? "
+
+name = input(prompt)
+print("Hello ," + name + "!")
