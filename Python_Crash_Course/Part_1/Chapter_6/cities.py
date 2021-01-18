6f043b813c46efcc8a1234b1f1a7f61722ed1647
doooghan <doooghan@foxmail.com>
2021-01-18 21:21:07 +0800

    dont ok

---
+++ Python_Crash_Course/Part_1/Chapter_6/cities.py
@@ -0,0 +1,22 @@
+cities = {
+    "xian": {
+        "country": "china",
+        "population": "2kw",
+        "fact": "rou jia mo",
+    },
+
+    "beijing": {
+        "country": "china",
+        "population": "3kw",
+        "fact": "gu gong",
+    },
+
+    "New York": {
+        "country": "USA",
+        "population": "3kw",
+        "fact": "big city"
+    },
+}
+
+for city in cities:
+    
