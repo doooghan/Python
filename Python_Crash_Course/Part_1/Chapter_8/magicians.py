78ca2d288716f2ce0ee5c5eee791e6038fde6804
doooghan <doooghan@foxmail.com>
2021-01-22 01:14:47 +0800

    magicians ok

--- Python_Crash_Course/Part_1/Chapter_8/magicians.py
+++ Python_Crash_Course/Part_1/Chapter_8/magicians.py
@@ -6,18 +6,29 @@ def show_magicians(magicians):
 
 def make_great(magicians):
     """每个魔术师都是伟大的"""
+    # 学到了什么,学到了可以对列表元素影响,但不能指向别的列表
     great_magicians = []
     while magicians:
         magician = magicians.pop()
         magician = "the Great " + magician
         great_magicians.append(magician)
+    return great_magicians
+    # return magicians.extend(great_magicians)[:]  # not ok
+    """ ok
+    magicians.extend(great_magicians)
+    return magicians
+    """
 
     """not ok
     for magician in magicians:
         magician = "the Great " + magician
     """
 
+
 magicians = ['hdd', 'yhh', 'hpp', 'wxx', 'znn', 'csh']
 show_magicians(magicians)
-make_great(magicians)
+print("-" * 20)
+great_magicians = make_great(magicians[:])
 show_magicians(magicians)
+print("-" * 20)
+show_magicians(great_magicians)
