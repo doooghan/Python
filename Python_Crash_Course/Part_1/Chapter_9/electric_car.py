ee5b698761232239c24977d03af4b1be436cc3b6
doooghan <doooghan@foxmail.com>
2021-01-27 21:36:21 +0800

    electric_car new fun

--- Python_Crash_Course/Part_1/Chapter_9/electric_car.py
+++ Python_Crash_Course/Part_1/Chapter_9/electric_car.py
@@ -35,13 +35,27 @@ class Car():
 class ElectricCar(Car):
     """电动汽车的独特之处"""
 
+    def __init__(self, make, model, year):
+        """
+        初始化父类的属性,再初始化电动汽车特有的属性
+        """
+        super().__init__(make, model, year)
+        self.battery_size = 70
+
     # def __init__(self, make, model, year):
     #     """初始化父类的属性"""
-    #     super().__init__(make, model, year)
-    def __init__(self, make, model, year):
-        """初始化父类的属性"""
-        Car.__init__(self, make, model, year)
+    #     Car.__init__(self, make, model, year)
+
+    def describe_battery(self):
+        """打印一条描述电瓶容量的消息"""
+        print("This car has a " + str(self.battery_size) + "-KWh battery")
+
+    def fill_gas_tank(self):
+        """电动汽车没有汽油"""
+        print("This car doesn't need a gas tank")
 
 
 my_tesla = ElectricCar('tesla', 'model s', 2016)
 print(my_tesla.get_descriptive_name())
+my_tesla.describe_battery()
+my_tesla.fill_gas_tank()
