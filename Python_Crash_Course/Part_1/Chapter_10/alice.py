fd2652224b7cd4350768f03d32994d5886a2b6d1
doooghan <doooghan@foxmail.com>
2021-02-01 15:02:57 +0800

    try...except

--- Python_Crash_Course/Part_1/Chapter_10/alice.py
+++ Python_Crash_Course/Part_1/Chapter_10/alice.py
@@ -6,3 +6,8 @@ try:
 except FileNotFoundError:
     msg = "Sorry, the file %s does not exist." % filename
     print(msg)
+else:
+    # 计算文件包含多少单词
+    words = contents.split()
+    number = len(words)
+    print("The file %s has about %s words." % (filename, number))
---
+++ Python_Crash_Course/Part_1/Chapter_10/word_count.py
@@ -0,0 +1,18 @@
+def count_words(filename):
+    """计算一个文件大概包含多少单词"""
+    try:
+        with open(filename) as f_obj:
+            contents = f_obj.read()
+    except FileNotFoundError:
+        msg = "Sorry, the file %s does not exist." % filename
+        print(msg)
+    else:
+        # 计算文件包含多少单词
+        words = contents.split()
+        number = len(words)
+        print("The file %s has about %s words." % (filename, str(number)))
+
+
+filenames = ['alice.txt', 'siddhartha.txt', 'moby_dick.txt', 'little_women.txt']
+for filename in filenames:
+    count_words(filename)
