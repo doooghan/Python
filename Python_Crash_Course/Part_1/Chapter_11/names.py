797acb2e0dbba82e1158e434dbaca88648614e20
doooghan <doooghan@foxmail.com>
2021-02-03 23:57:10 +0800

    test

---
+++ Python_Crash_Course/Part_1/Chapter_11/names.py
@@ -0,0 +1,12 @@
+from Python_Crash_Course.Part_1.Chapter_11.name_function import get_formatted_name
+
+print("Enter 'q' at any time to quit.")
+while True:
+    first = input("\nPlease give me a first name: ")
+    if first == 'q':
+        break
+    last = input("Please give me a last name: ")
+    if last == 'q':
+        break
+    formatted_name = get_formatted_name(first, last)
+    print("\tNeatly formatted name: " + formatted_name + '.')
