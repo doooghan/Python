c1bb047e14761518a5f26502af0dc5385e66c22e
doooghan <doooghan@foxmail.com>
2021-02-08 23:59:59 +0800

    matplolib

---
+++ mpl_squares.py
@@ -0,0 +1,13 @@
+import matplotlib.pylab as plt
+
+squares = [1, 4, 9, 16, 25]
+plt.plot(squares, linewide=5)
+
+# 设置图表标题,并给坐标轴加上标签
+plt.title("Squares Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("square of Value", fontsize=14)
+
+# 设置刻度标记的大小
+plt.tick_params(axis="both", labelsize=14)
+plt.show()
