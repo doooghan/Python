484cdb38f43728086466ad5d521e9e2f832b9593
doooghan <doooghan@foxmail.com>
2021-02-17 23:52:59 +0800

    color

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
-plt.scatter(x_values, y_values, s=200)
+x_values = list(range(1, 1001))
+y_values = [x ** 2 for x in x_values]
+plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, edgecolor='none', s=40)
+# plt.scatter(x_values, y_values, c=(0, 0, 0.8), edgecolor='none', s=40)
+# plt.scatter(x_values, y_values, c='red', edgecolor='none', s=40)
 
 # 设置图表标题,并给坐标轴加上标签
 plt.title("Squares Numbers", fontsize=24)
@@ -10,6 +12,9 @@ plt.xlabel("Value", fontsize=14)
 plt.ylabel("square of Value", fontsize=14)
 
 # 设置刻度标记的大小
-plt.tick_params(axis="both", which="major", labelsize=14)
+# plt.tick_params(axis="both", which="major", labelsize=14)
+
+# 设置每个坐标轴的取值范围
+plt.axis([0, 1100, 0, 1100000])
 
 plt.show()
