86d3cd17dec060e650ead565e637a1800bb1b387
doooghan <doooghan@foxmail.com>
2021-02-19 23:58:52 +0800

    plt axes

--- rw_visual.py
+++ rw_visual.py
@@ -5,16 +5,20 @@ from random_walk import RandomWalk
 # 只要程序处于活动状态就,就不断的模拟随机漫步
 while True:
     # 创建一个 RandomWalk 实例,并将其所包含的点都绘制出来
-    rw = RandomWalk()
+    rw = RandomWalk(10000)
     rw.fill_walk()
 
     point_numbers = list(range(rw.num_points))
-    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors="none", s=4)
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors="none", s=1)
     # plt.scatter(rw.x_values, rw.y_values, s=4)
 
     # 突出起点和终点
     plt.scatter(0, 0, c="green", edgecolors="none", s=100)
     plt.scatter(rw.x_values[-1], rw.y_values[-1], c="red", edgecolors="none", s=100)
+
+    # 隐藏坐标轴
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
 
     plt.show()
 
