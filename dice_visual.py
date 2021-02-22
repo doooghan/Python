29ea11704c03c9b8c4888878cd2379e5f34ca56b
doooghan <doooghan@foxmail.com>
2021-02-22 23:36:56 +0800

    die all

--- dice_visual.py
+++ dice_visual.py
@@ -4,11 +4,11 @@ from die import Die
 
 # 创建两个 D6 骰子
 die1 = Die()
-die2 = Die()
+die2 = Die(10)
 
 # 掷骰子多次，并将结果存储到一个列表中
 results = []
-for roll_num in range(1000):
+for roll_num in range(50000):
     result = die1.roll() + die2.roll()
     results.append(result)
 
@@ -22,10 +22,10 @@ for value in range(2, max_result + 1):
 # 可视化结果
 hist = pygal.Bar()
 
-hist.title = "Results of rolling two D6 dice 1000 times."
-hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+hist.title = "Results of rolling a D6 and a D10 50,000 times."
+hist.x_labels = [str(i) for i in range(2, max_result + 1)]
 hist.x_title = "Result"
 hist.y_title = "Frequency of result"
 
-hist.add("D6 + D6", frequencies)
+hist.add("D6 + D10", frequencies)
 hist.render_to_file("dice_visual.svg")
--- die_visual.py
+++ die_visual.py
@@ -25,9 +25,9 @@ for value in range(1, die.num_sides + 1):
 hist = pygal.Bar()
 
 hist.title = "Results of rolling one D6 1000 times."
-hist.x_labels = ["1", "2", "3", "4", "5", "6"]
+hist.x_labels = [str(i) for i in range(1, die.num_sides + 1)]
 hist.x_title = "Result"
 hist.y_title = "Frequency of result"
 
 hist.add("D6", frequencies)
-hist.render_to_file("die_visual.png")
+hist.render_to_file("die_visual.svg")
