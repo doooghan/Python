8903975b9688d9edd54f59777422adb8bf5a3b0f
doooghan <doooghan@foxmail.com>
2021-02-27 11:03:09 +0800

    x_label

--- highs_lows.py
+++ highs_lows.py
@@ -1,5 +1,5 @@
 import csv
-
+from datetime import datetime
 from matplotlib import pyplot as plt
 
 # 从文件中获取最高气温
@@ -11,8 +11,10 @@ with open(filename) as f:
     # for index, column_header in enumerate(header_row):
     #     print(index, column_header)
 
-    highs = []
+    dates, highs = [], []
     for row in reader:
+        current_date = datetime.strptime(row[0], "%Y-%m-%d")
+        dates.append(current_date)
         high = int(row[1])
         highs.append(high)
 
@@ -20,11 +22,12 @@ with open(filename) as f:
 
 # 根据数据绘制图形
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(highs, c="red")
+plt.plot(dates, highs, c="red")
 
 # 设置图形的格式
 plt.title("Daily high temperatures, July 2014", fontsize=24)
 plt.xlabel("", fontsize=16)
+fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 
