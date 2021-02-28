ea9b382988a9b7fb35ce857ef87ff73951275b08
doooghan <doooghan@foxmail.com>
2021-02-28 22:49:15 +0800

    world populations color

--- world_population.py
+++ world_population.py
@@ -1,5 +1,6 @@
 import json
 
+from pygal.style import RotateStyle, LightColorizedStyle
 from pygal_maps_world.maps import World
 from country_codes import get_country_code
 
@@ -21,8 +22,27 @@ for pop_dict in pop_data:
         # else:
         #     print("ERROR - %s" % country_name)
 
-wm = World()
+# 根据人口数量将所有的国家分成三组
+cc_pops_1, cc_pops_2, cc_pops_3 = {}, {}, {}
+for cc, pop in cc_populations.items():
+    if pop < 10000000:
+        cc_pops_1[cc] = pop
+    elif pop < 1000000000:
+        cc_pops_2[cc] = pop
+    else:
+        cc_pops_3[cc] = pop
+
+# 看看每组分别包含多少个国家
+print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
+
+# wm_style = RotateStyle("#336699")
+# wm_style = LightColorizedStyle
+wm_style = RotateStyle("#336699", base_style=LightColorizedStyle)
+wm = World(style=wm_style)
 wm.title = "World Population in 2010, by Country"
-wm.add("2010", cc_populations)
+wm.add("0-10m", cc_pops_1)
+wm.add("10m-1bn", cc_pops_2)
+wm.add(">1bn", cc_pops_3)
+# wm.add("2010", cc_populations)
 
 wm.render_to_file("world_populations.svg")
