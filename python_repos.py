cc91eea69dfb8fd563f0a2b06479aa2a23ccd5fc
doooghan <doooghan@foxmail.com>
2021-03-04 16:57:12 +0800

    api python over

--- python_repos.py
+++ python_repos.py
@@ -14,10 +14,17 @@ print("Total repositories:", response_dict["total_count"])
 
 # 探索有关仓库的信息
 repo_dicts = response_dict["items"]
-names, stars = [], []
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
+    # print(repo_dict["name"])
     names.append(repo_dict["name"])
-    stars.append(repo_dict["stargazers_count"])
+
+    plot_dict = {
+        "value": repo_dict["stargazers_count"],
+        "label": repo_dict["description"] or "",
+        "xlink": repo_dict["html_url"]
+    }
+    plot_dicts.append(plot_dict)
 
 # 可视化
 my_style = LS("#333366", base_style=LCS)
@@ -36,5 +43,5 @@ chart = pygal.Bar(style=my_style, config=my_config)
 chart.title = "Most-Starred Python Projects on GitHub"
 chart.x_labels = names
 
-chart.add("", stars)
+chart.add("", plot_dicts)
 chart.render_to_file("python_repos.svg")
